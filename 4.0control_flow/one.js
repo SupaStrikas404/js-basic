8560187d82b8a8a82c3c2c48d58663a9e7f6584d
Kabir <157941331+SupaStrikas404@users.noreply.github.com>
2024-03-23 15:43:00 +0000

    Learning in process

---
+++ 4.0control_flow/one.js
@@ -0,0 +1,29 @@
+// IF 
+
+const temp = 23
+// if (temp === 25) {
+//     console.log("Temperature is normal");
+// }
+// else{
+//     console.log("Temperature is not stable.");
+// }
+
+const age = 21
+// if (age >= 18) {
+//     const component = "helmet"
+//     console.log(`You can Drive with ${component}.`);
+// }
+
+const balance = 5000
+if (balance>=10000) {
+    
+}
+
+// In "IF" statement if, the condition is "false" code will not execute.  
+// if (false) {
+//     console.log("Hi there");
+// }
+
+// Operator : >, <, >=, <=, ==(It checks only Value), !=, ===(It checks Datatype), !== 
+
+// var is a Global Scope. 
