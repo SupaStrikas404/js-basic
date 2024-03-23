d87891548e892da3024d290309d0fe9582f58aa7
Kabir <157941331+SupaStrikas404@users.noreply.github.com>
2024-03-23 15:53:27 +0000

    Still in process

--- 4.0control_flow/one.js
+++ 4.0control_flow/one.js
@@ -1,24 +1,5 @@
 // IF 
 
-const temp = 23
-// if (temp === 25) {
-//     console.log("Temperature is normal");
-// }
-// else{
-//     console.log("Temperature is not stable.");
-// }
-
-const age = 21
-// if (age >= 18) {
-//     const component = "helmet"
-//     console.log(`You can Drive with ${component}.`);
-// }
-
-const balance = 5000
-if (balance>=10000) {
-    
-}
-
 // In "IF" statement if, the condition is "false" code will not execute.  
 // if (false) {
 //     console.log("Hi there");
@@ -27,3 +8,41 @@ if (balance>=10000) {
 // Operator : >, <, >=, <=, ==(It checks only Value), !=, ===(It checks Datatype), !== 
 
 // var is a Global Scope. 
+
+
+
+const temp = 23
+// if (temp === 25) {
+//     console.log("Temperature is normal");
+// }
+// else{
+//     console.log("Temperature is not stable.");
+// }
+
+
+const age = 21
+// if (age >= 18) {
+//     const component = "helmet"
+//     console.log(`You can Drive with ${component}.`);
+// }
+
+
+// // Bad approach of writing code
+// const balance = 6000
+// if (balance>5000) console.log("you have sufficient balance."), console.log(`Total amount : ${balance}`);
+
+
+const num = 1
+// if (num > 3) {
+//     console.log("Test1 Pass");
+// }
+// else if (num > 5) {
+//     console.log("Test2 Pass");
+// }
+// else if (num > 6) {
+//     console.log("Test3 Pass");
+// } else {
+//     console.log("Number is less then 10.");
+// }
+
+
