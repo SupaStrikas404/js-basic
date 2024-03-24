45b0d6959091f15eb297d012eb496a9e19d9d0ce
Kabir <157941331+SupaStrikas404@users.noreply.github.com>
2024-03-24 18:54:40 +0000

    For loop done.

--- 5.0iterations/for_loop.js
+++ 5.0iterations/for_loop.js
@@ -8,19 +8,40 @@
 //     console.log(element);
 // }
 
-
+/*
 for (let i = 1; i <= 10; i++) {
     // console.log(`Class of: ${i}`);
-    // console.log(`Table of: ${i}`);
+    console.log(`Table of: ${i}`);
     for (let j = 1; j <= 10; j++) {
         // console.log(`Class ${i} Roll No. ${j}`);
-        // console.log(`${i} * ${j} = ` + i*j);
+        console.log(`${i} * ${j} = ` + i*j);
     }
 }
+*/
+
+const myFrnd=["Ram", "Sam", "Jadu", "Momo", "Bora"]
+
+// for (let i = 0; i < myFrnd.length; i++) {
+//     const ans = myFrnd[i];
+//     console.log(ans);
+// }
+
+// Break and Continue 
+ 
+// for (let i = 0; i < myFrnd.length; i++) {
+//     console.log(myFrnd[i]);
+//     if (myFrnd[i] == "Jadu") {
+//         console.log(`Jadu found.`);
+//         break
+//     }
+// }
+// console.log(`Jadu is Gaddar.`);
 
 
-const myFrnd=["Ram", "Sam", "Jadu", "Momo"]
 for (let i = 0; i < myFrnd.length; i++) {
-    const ans = myFrnd[i];
-    console.log(ans);
+    if (myFrnd[i] == "Jadu") {
+        console.log(`Skip It...`);
+        continue
+    }
+    console.log(myFrnd[i]);
 }
